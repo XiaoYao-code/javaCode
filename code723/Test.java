cca8918f3246e5ccf13cf23d203c0e05af9591ad
姚鑫原 <1774164628@qq.com>
2020-07-23 23:52:07 +0800

    ==和equals

---
+++ code723/Test.java
@@ -0,0 +1,59 @@
+package code723;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import sun.java2d.pipe.AAShapePipe;
+
+class A{
+    String name= "a";
+
+
+}
+public class Test {
+    public static void main(String[] args) {
+        A A1= new A();
+        A A2= new A();
+        System.out.println(A1.equals(A2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*Integer a=200;
+        Integer b=200;
+        System.out.println(a==b);
+        System.out.println(a.equals(b));//
+        /*int a=1;
+        int b=1;
+        int c=128;
+        Integer a1=1;
+        Integer b1=1;
+        Integer a2=-128;
+        Integer b2=-128;
+        Integer a3=128;
+        Integer b3=128;
+        System.out.println(a==b);
+        System.out.println(a1==b1);
+        System.out.println(a2==b2);
+        System.out.println(a3==b3);
+        System.out.println(a==a1);
+        System.out.println(c==a3);*/
+
+
+
+
+
+
+    }
+}
