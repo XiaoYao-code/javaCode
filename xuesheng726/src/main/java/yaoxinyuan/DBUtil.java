991ae0a122d66a619fb9266621e44e9e5d0ffc14
姚鑫原 <1774164628@qq.com>
2020-07-26 22:09:51 +0800

    项目

---
+++ xuesheng726/src/main/java/yaoxinyuan/DBUtil.java
@@ -0,0 +1,18 @@
+package yaoxinyuan;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DBUtil {
+    public static void close(Connection c, Statement s, ResultSet r){
+        if (c!=null){
+
+        }
+        if (s!=null);{
+
+        }
+        }
+    }
+
+
