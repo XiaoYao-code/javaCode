2dc9e747904b2329288b917a7e658c0f1d15b564
姚鑫原 <1774164628@qq.com>
2020-08-28 23:21:23 +0800

    oj

---
+++ code828/OJ.java
@@ -0,0 +1,18 @@
+package code622;
+
+import java.io.File;
+import java.io.IOException;
+
+public  class Test {
+    public static void main(String[] args) throws IOException {
+        String  name="D:\\062.java";
+        File  file=new File(name);
+        System.out.println(file.getAbsolutePath());
+
+
+       boolean newFile=file.createNewFile();
+        System.out.println(newFile);
+
+    }
+
+}
