f59cad925ca5af54d833b009aef4f7c5909586d3
姚鑫原 <1774164628@qq.com>
2020-10-11 23:44:30 +0800

    test

---
+++ code1011/test.java
@@ -0,0 +1,34 @@
+package code509;
+public class TestDemo {
+  /*  String str = new String("good");
+    char[ ] ch = { 'a' , 'b' , 'c' };
+    public static void main(String args[]){
+        TestDemo ex = new TestDemo();
+        ex.change(ex.str,ex.ch);
+        System.out.print(ex.str + " and ");
+        System.out.print(ex.ch);
+    }
+    public void change(String str,char ch[ ]){
+        str = "test ok";
+        ch[0] = 'g';
+    }*/
+/*  public static boolean isAdmin(String userId){
+      System.out.println(userId.toLowerCase());
+      return userId.toLowerCase()=="admin";
+  }
+    public static void main(String[] args){
+        *//*System.out.println(isAdmin("Admin"));*//*
+    }*/
+/*  public static void main(String[] args) {
+      String s1="abc"+"def";//1
+      String s2=new String(s1);//2
+      if(s1.equals(s2))//3
+          System.out.println(".equals succeeded");//4
+      if(s1==s2)//5
+          System.out.println("==succeeded");//6
+  }*/
+
+  public static void main(String[] args) {
+
+  }
+}
